94cb5370d1d9932f8bdf5bd823d7357985e16d92
huwenguang <huwenguang@ebankunion.com>
2019-12-20 16:02:54 +0800

    add example Flux to Mono

--- src/main/java/com/ljm/reactor/operators/PRStartwith.java
+++ src/main/java/com/ljm/reactor/operators/PRStartwith.java
@@ -1,5 +1,8 @@
 package com.ljm.reactor.operators;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 /**
  * ¨€¨€¨[  ¨€¨€¨[¨€¨€¨[    ¨€¨€¨[ ¨€¨€¨€¨€¨€¨€¨[
  * ¨€¨€¨U  ¨€¨€¨U¨€¨€¨U    ¨€¨€¨U¨€¨€¨X¨T¨T¨T¨T¨a
@@ -10,4 +13,25 @@ package com.ljm.reactor.operators;
  * Created by huwenguang on 2019/12/2.
  */
 public class PRStartwith {
+    public Mono<Integer> convertFlux2Mono(){
+        //region
+        /***
+         *
+         // https://stackoverflow.com/questions/42021559/convert-from-flux-to-mono
+
+         How can I convert from a Flux with 1 element to a Mono?
+         Flux.fromArray(arrayOf(1,2,1,1,1,2))
+         .distinct()
+         .take(1)
+         How do I make this a Mono(1)?
+         */
+        //endregion
+
+        Mono<Integer> fluxInt = Flux.just(1,2,1,1,1,2)
+                .distinct()
+                .take(1).singleOrEmpty();
+
+
+        return fluxInt;
+    }
 }
